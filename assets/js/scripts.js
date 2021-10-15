55a28266c131890d8873327f11f73667576ff08e
Dan Zins <danzins15@gmail.com>
2021-10-15 15:22:07 -0500

    Completed Question project but adapting selector inside element instead of traversing the DOM approach

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,2 +1,40 @@
-//using selectors inside the element
-// traversing the dom
+// Utilizing selectors inside the element instead of traversing the DOM to find a specific element
+
+// Retrieving article elements
+const questions = document.querySelectorAll('.question');
+
+// Looping article elements with forEach
+questions.forEach(function(question){
+    // Here you are looking through the article element with the class question instead of looking through the DOM and selecting the btn classes question-btn
+    const btn = question.querySelector('.question-btn');
+    
+    btn.addEventListener('click', function(){
+       
+        questions.forEach(function(item){
+        
+            if(item !== question){
+            item.classList.remove('show-text');
+        }
+       });
+       
+       question.classList.toggle('show-text');
+    });
+
+});
+
+
+
+
+
+// Second method of traversing the DOM to execute click button action
+
+// Retrieve all question btns
+// const btns = document.querySelectorAll('.question-btn');
+
+// Executing forEach loop for multiple button classes and add click event
+// btns.forEach(function(btn){
+//     btn.addEventListener('click', function(e){
+//         const question = e.currentTarget.parentElement.parentElement;
+//         question.classList.toggle('show-text');
+//     });
+// });
